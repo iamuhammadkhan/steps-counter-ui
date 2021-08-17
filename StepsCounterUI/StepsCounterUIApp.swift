4be31d705cc8a42d0375b0cc0400027e29be56a3
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-17 16:48:15 +0500

    Initial Commit

---
+++ StepsCounterUI/StepsCounterUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsCounterUIApp.swift
+//  StepsCounterUI
+//
+//  Created by Muhammad Khan on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct StepsCounterUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
